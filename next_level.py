c833d2cd0f5fa916ec9fa94391dd74374aa658e9
KAMARAJ GANESAN <50704113+RaJ173@users.noreply.github.com>
2021-04-08 12:28:28 +0530

    Update next_level.py

--- next_level.py
+++ next_level.py
@@ -29,8 +29,8 @@ def main():
 
     @st.cache(persist=True)
     def split(df):
-        y = df.dtypes
-        x = df.drop(columns=['dtypes'])
+        y = df.type
+        x = df.drop(columns=['type'])
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
         return x_train, x_test, y_train, y_test
